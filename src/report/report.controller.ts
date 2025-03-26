61258e0623f3e880ac75144da89ce087b8aff3e7
v1po <overcross1337@gmail.com>
2025-03-26 18:33:34 +0700

    Update report.controller.ts

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -13,22 +13,14 @@ export class ReportsController {
     if (!report) {
       throw new NotFoundException('Отчет не найден');
     }
-
-    // Создание нового workbook и worksheet
     const workbook = xlsx.utils.book_new();
     const worksheetData = [
       ['Дата', 'Ожидание семьи', 'Задачи', 'Рекомендации', 'Воплощение рекомендаций', 'Результаты'],
       [report.createdAt, 'Диаграмма с данными', '', report.recommendations, '', '']
     ];
     const worksheet = xlsx.utils.aoa_to_sheet(worksheetData);
-
-    // Добавление worksheet в workbook
     xlsx.utils.book_append_sheet(workbook, worksheet, 'Report');
-
-    // Генерация XLSX файла
     const buffer = xlsx.write(workbook, { type: 'buffer', bookType: 'xlsx' });
-
-    // Установка заголовков и отправка файла на скачивание
     res.setHeader('Content-Disposition', `attachment; filename=report_${id}.xlsx`);
     res.setHeader('Content-Type', 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
     res.send(buffer);
