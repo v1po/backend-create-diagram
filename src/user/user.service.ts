784d4c4c56f39b7b8cbd9950fc612c2652ff03da
v1po <overcross1337@gmail.com>
2025-03-26 18:37:25 +0700

    Update user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -15,30 +15,26 @@ export class UserService {
     @InjectRepository(Survey)
     private surveyRepository: Repository<Survey>,
   ) {}
-
+  
   async findOrCreate(firstName: string, lastName: string): Promise<User> {
-    this.logger.log(`Finding or creating user: ${firstName} ${lastName}`);  // Логирование
+    this.logger.log(`Finding or creating user: ${firstName} ${lastName}`);  
     let user;
     try {
       if (!firstName || !lastName) {
         throw new Error('First name and last name must be provided');
       }
-
-      // Очистка данных перед запросом
       const cleanedFirstName = firstName.trim();
       const cleanedLastName = lastName.trim();
-
-      this.logger.log(`Cleaned firstName: ${cleanedFirstName}, Cleaned lastName: ${cleanedLastName}`);  // Логирование очищенных данных
-
+      this.logger.log(`Cleaned firstName: ${cleanedFirstName}, Cleaned lastName: ${cleanedLastName}`); 
       user = await this.userRepository.findOne({ where: { firstName: cleanedFirstName, lastName: cleanedLastName } });
       if (!user) {
-        this.logger.log('User not found, creating new user');  // Логирование
+        this.logger.log('User not found, creating new user');  
         user = this.userRepository.create({ firstName: cleanedFirstName, lastName: cleanedLastName });
         await this.userRepository.save(user);
       }
-      this.logger.log('User found or created:', user);  // Логирование
+      this.logger.log('User found or created:', user);  
     } catch (error) {
-      this.logger.error('Error in findOrCreate:', error);  // Логирование ошибок
+      this.logger.error('Error in findOrCreate:', error);  
       throw error;
     }
     return user;
