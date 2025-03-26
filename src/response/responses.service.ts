7623bc776fa753e53299ffc3dc4557d9a4b3d77c
v1po <overcross1337@gmail.com>
2025-03-26 18:35:42 +0700

    Update responses.service.ts

--- src/response/responses.service.ts
+++ src/response/responses.service.ts
@@ -9,14 +9,6 @@ export interface Responses {
 }
 
 const responses: { [key: number]: Responses } = {
-  1: {
-    year: 2023,
-    labels: ['Label 1', 'Label 2', 'Label 3'], // Замените на реальные метки
-    studentData: [85, 90, 78],
-    parentData: [88, 85, 92],
-    teacherData: [75, 80, 85],
-  },
-  // Добавьте остальные записи ответов здесь
 };
 
 @Injectable()
