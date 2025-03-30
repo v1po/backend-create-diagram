190e2e508e264b3ceb93dc6b3440856adcdb88f6
v1po <overcross1337@gmail.com>
2025-03-30 22:14:08 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: '*', 
+    origin: '*',
     methods: 'GET,HEAD,POST,PUT,PATCH,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
     credentials: true,
   });
 
-  await app.listen(3000); 
+  await app.listen(process.env.PORT || 3000, '0.0.0.0'); // 🚀 Railway-friendly
 }
 bootstrap();
